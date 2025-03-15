86b6ab7fa2c361d1eab8b0d26740bd19c8622924
Minushika Kapuwaththa <104319692+MinushikaKapuwaththa@users.noreply.github.com>
2025-03-15 23:25:53 +0530

    Resolve merge conflicts in Hero component and update package.json dependencies

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import img2 from '../../assets/hero/mypic3.PNG'
-=======
-import img2 from '../../assets/hero/my pic 3.jpg'
->>>>>>> c7e9244eab4945fd84da139d3ce88c55e3c35a43
 import './Hero.css'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -12,7 +8,6 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Hero() {
   return (
-<<<<<<< HEAD
    <section className='hero-container'>
     <div className='hero-content'>
         <h1>Hi! I'm </h1> 
@@ -46,27 +41,6 @@ function Hero() {
     </div>
     </div>
    </section>
-=======
-    <section className="home">
-        <div className='home-info'>
-            <h1>Hi I'm Minushika kapuwaththa</h1>
-            <h2>I'm Quality Assurance Engineer</h2>
-            <p>I am a final-year undergraduate at the University of Moratuwa, pursuing a degree in Information Technology and Management. Passionate about exploring new technologies, I approach tasks with maturity and responsibility, applying my knowledge through self-study and hard work to continuously grow in the field</p>
-
-            <div className='btn-sci'>
-                <a href='#'className='btn'>Download CV</a>
-                <div className='sci'>
-                    <a href='#'></a>
-                    <a href='#'></a>
-                    <a href='#'></a>
-                    <a href='#'></a>
-            </div>
-
-        </div>
-        </div>
-        </section>
-  
->>>>>>> c7e9244eab4945fd84da139d3ce88c55e3c35a43
   )
 }
 
