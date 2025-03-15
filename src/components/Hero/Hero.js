514165295a5172c0a329bd2c6d0a7ce1d36bc1b7
Minushika Kapuwaththa <104319692+MinushikaKapuwaththa@users.noreply.github.com>
2025-03-15 23:06:08 +0530

    updated hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,28 +1,43 @@
 import React from 'react'
-import img2 from '../../assets/img2.png'
+import img2 from '../../assets/hero/mypic3.PNG'
 import './Hero.css'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Hero() {
   return (
    <section className='hero-container'>
     <div className='hero-content'>
-        <h1>Hi! I'm Minushika Kapuwaththa </h1>
-        <p>I am a final-year undergraduate at the University of Moratuwa, pursuing a degree in Information Technology and Management. Passionate about exploring new technologies, I approach tasks with maturity and responsibility, applying my knowledge through self-study and hard work to continuously grow in the field
+        <h1>Hi! I'm </h1> 
+        <h1><span> Minushika Kapuwaththa </span></h1>
+        {/* <h2>I'm Quality Assurance Engineer</h2> */}
+        <p>Final-year undergraduate at the University of Moratuwa, pursuing a degree in Information Technology and Management. Passionate about exploring new technologies, I approach tasks with maturity and responsibility, applying my knowledge through self-study and hard work to continuously grow in the field
 
         </p>
-
-        {/* <button className='conbtn'>Resume</button> */}
+<div className='btn-sci'>
+        <a href="#" className='btn'>Download CV</a>
         
-        
-    </div>
-
-    <div className='hero-img'>
-        <div>
-            <div className='tech-icon'>
-                <img src={ img2} alt="Profile"/>
-            </div>
+        <div className='social-icons'>
+            <a href="https://www.linkedin.com/in/minushika-kapuwaththa-9b8a7b1a4/">
+                <LinkedInIcon />
+            </a>
+            <a href="https://github.com/MinushikaKapuwaththa">
+                 <GitHubIcon  /> 
+            </a>
+            <a href="https://web.facebook.com/Minushika.kapuwaththa">
+                <FacebookIcon /> 
+            </a>
+            
         </div>
-
+        </div>  
+    </div>
+    <div className='hero-img'>
+        <div className='img-box'>
+            <div className='img-item'>
+        <img src={img2} alt='hero-img'/>
+        </div>
+    </div>
     </div>
    </section>
   )
